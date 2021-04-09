21471218a788124a24319a346c240c6e0b3a7d2e
Andrew_myz2001 <979148792@qq.com>
2021-04-09 09:57:55 +0800

    加入了线性回归预测

--- Experiment/Data_regression.py
+++ Experiment/Data_regression.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
 data = pd.read_csv('advertising.csv')
 x = data[['TV', 'Radio', 'Newspaper']]
 y = data['Sales']
@@ -20,3 +25,24 @@ plt.title('Newspaper')
 plt.grid()
 plt.tight_layout()
 plt.show()
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+linreg = LinearRegression()
+model = linreg.fit(x_train, y_train)
+print(model)
+print(linreg.coef_)
+print(linreg.intercept_)
+
+y_hat = linreg.predict(np.array(x_test))
+mse = np.average((y_hat - np.array(y_test)) ** 2)  # Mean Squared Error
+rmse = np.sqrt(mse)  # Root Mean Squared Error
+print(mse, rmse)
+
+t = np.arange(len(x_test))
+plt.plot(t, y_test, 'r-', linewidth=2, label='Test')
+plt.plot(t, y_hat, 'g-', linewidth=2, label='Predict')
+plt.legend(loc='upper right')
+plt.grid()
+plt.show()
+
+# todo 应该把原理搞清楚
