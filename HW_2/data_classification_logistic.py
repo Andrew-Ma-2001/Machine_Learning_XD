d6d84c78b92159302cffbd15375f82cb1c056da9
Andrew_myz2001 <979148792@qq.com>
2021-04-10 15:20:35 +0800

    重构格式

--- HW_2/data_classification_logistic.py
+++ HW_2/data_classification_logistic.py
@@ -90,25 +90,26 @@ class Logistic_regression():
         return accuracy
 
 
-# 导入数据集iris  
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-iris = pandas.read_csv(url, names=names)  # 读取csv数据
+if __name__ == '__main__':
+    # 导入数据集iris  
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    iris = pandas.read_csv(url, names=names)  # 读取csv数据
 
-# 数据预处理
-X = iris[['sepal-length', 'sepal-width', 'petal-length', 'petal-width']]
-X = X[0:100]
-y = iris['class']
-y = y[0:100]
-encoder = LabelEncoder()
-y = encoder.fit_transform(y)
-train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=101)
+    # 数据预处理
+    X = iris[['sepal-length', 'sepal-width', 'petal-length', 'petal-width']]
+    X = X[0:100]
+    y = iris['class']
+    y = y[0:100]
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(y)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=101)
 
-model = LogisticRegression()
-model.fit(train_X, train_y)
-prediction = model.predict(test_X)
-print('The accuracy of the Logistic Regression is: {0}'.format(metrics.accuracy_score(prediction, test_y)))
+    model = LogisticRegression()
+    model.fit(train_X, train_y)
+    prediction = model.predict(test_X)
+    print('The accuracy of the Logistic Regression is: {0}'.format(metrics.accuracy_score(prediction, test_y)))
 
-model = Logistic_regression(x_train=train_X, y_train=train_y, x_test=test_X, y_test=test_y)
-model.epochs = 50
-model.accTest()
+    model = Logistic_regression(x_train=train_X, y_train=train_y, x_test=test_X, y_test=test_y)
+    model.epochs = 50
+    model.accTest()
